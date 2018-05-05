a4babdd5309ad4e7bf28740b2baaaa49a80a25fa
mawad8 <mawad8@yahoo.com>
2018-05-04 21:56:53 -0700

    BOOTSTRAP

---
+++ food-proj/js/main.js
@@ -0,0 +1,26 @@
+function myFunction() {
+	// body...
+}
+function ing() {
+	var foode = document.getElementById('food');
+	var teto = document.createTextNode("1 - 2 lbs lean lamb (1-1/2 inch cubes, beef can be substituted)");
+foode.appendChild(teto);
+	var foode1 = document.getElementById('food1');
+	var teto1 = document.createTextNode("2- 1⁄2 cup clarified butter (use samna or ghee).");
+	foode1.appendChild(teto1);
+	var foode2 = document.getElementById('food2');
+	var teto2 = document.createTextNode("3- 1 medium onion, finely chopped.");
+	foode2.appendChild(teto2);
+	var foode3 = document.getElementById('food3');
+	var teto3 = document.createTextNode("4- 4 cups plain Greek yogurt.");
+	foode3.appendChild(teto3);
+	var foode4 = document.getElementById('food4');
+	var teto4 = document.createTextNode("5- 1 egg white.");
+	foode4.appendChild(teto4);
+	var foode5 = document.getElementById('food5');
+	var teto5 = document.createTextNode("6- 1 teaspoon pepper")
+	foode5.appendChild(teto5);
+	
+
+
+}
